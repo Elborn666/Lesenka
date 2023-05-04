e5fb88b74605193163c1e04b6941817ff9e05fad
Elborn666 <108280320+Elborn666@users.noreply.github.com>
2023-05-04 18:41:43 +0300

    сделал скрипт для policy, исправил ошибки

--- js/policy.js
+++ js/policy.js
@@ -1,2 +1,19 @@
-const refPolicy = document.querySelector('.policy')
+const refsPolicy = document.querySelectorAll('.js-policy');
+const btnClose = document.querySelector('.modal__close-btn');
+const refModal = document.querySelector('.backdrop');
 
+btnClose.addEventListener('click', onClosePolicy)
+
+for (let refPolicy of refsPolicy) {
+  refPolicy.addEventListener('click', onClickPolicy);
+}
+
+function onClickPolicy(e){
+  e.preventDefault()
+  refModal.classList.add('is-block')
+}
+
+function onClosePolicy(e){
+  e.preventDefault()
+    refModal.classList.remove('is-block')
+}
